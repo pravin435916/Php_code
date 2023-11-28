039bade535c9c74f53886db1913430f823e08e35
pravin435916 <pravinnandankar03@gmail.com>
2023-11-28 12:03:07 +0530

    changes title and other

--- app/View/Components/Layouts/Main.php
+++ app/View/Components/Layouts/Main.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\View\Components\Layouts;
 
 use Closure;
@@ -8,19 +7,24 @@ use Illuminate\View\Component;
 
 class Main extends Component
 {
+    public string $pageName;
+    public string $title = "Playlist App";
     /**
      * Create a new component instance.
      */
     public function __construct()
     {
-        //
+        $this->pageName = $pageName;
     }
-
+    
     /**
      * Get the view / contents that represent the component.
      */
     public function render(): View|Closure|string
     {
+        if($this->pageName){
+            $this->title = $this->title.' | '.$this->pageName;
+          }
         return view('components.layouts.main');
     }
 }
