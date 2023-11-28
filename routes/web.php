baed2bda973d64e1220d67da8915e37e9aa1126f
pravin435916 <pravinnandankar03@gmail.com>
2023-11-28 11:20:54 +0530

    Update songs view to read from database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use Practicals\Song;
+// use Practicals\Song;
+use App\Models\Song;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,21 +11,24 @@ Route::get('/songs_static', function () {
     return view('songs_static');
   });
   
+  // Route::get('/songs', function () {
+  //       $song1 = new Song();
+  //   $song1->setTitle("Stan");
+  //   $song1->setArtist("Eminem");
+  
+  //   $song2 = new Song();
+  //   $song2->setTitle("New Song");
+  //   $song2->setArtist("Pravin");
+  
+  //   $song3 = new Song();
+  //   $song3->setTitle("With You");
+  //   $song3->setArtist("A P Dhillon");
+  
+  //   return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  // });
   Route::get('/songs', function () {
-        $song1 = new Song();
-    $song1->setTitle("Stan");
-    $song1->setArtist("Eminem");
-  
-    $song2 = new Song();
-    $song2->setTitle("New Song");
-    $song2->setArtist("Pravin");
-  
-    $song3 = new Song();
-    $song3->setTitle("With You");
-    $song3->setArtist("A P Dhillon");
-  
-    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
-  });
+    return view('songs', [ 'songs' => Song::all() ] );
+});
 
 Route::get('/veggies', function () {
     return view('veggies');
